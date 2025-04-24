b9563b5106a48aee59276c531ba4894105ef45d9
Lypsil0n <weeqoo789@gmail.com>
2025-04-24 19:41:48 +0200

    Fix fixture

--- backend/test/integration/test_dao.py
+++ backend/test/integration/test_dao.py
@@ -2,10 +2,15 @@ import pytest
 from src.util.dao import DAO
 from pymongo.errors import WriteError
 
-dao = DAO("user")
+@pytest.fixture
+def dao():
+    dao_instance = DAO("user")
+    yield dao_instance
+
+    dao_instance.collection.delete_one({'email': 'jane.doe@gmail.com'})
 
 @pytest.mark.integration
-def test_create_valid():
+def test_create_valid(dao):
     data = [('firstName', 'Jane'), ('lastName', 'Doe'), ('email', 'jane.doe@gmail.com')]
     obj = dao.create(data)
     
@@ -14,14 +19,14 @@ def test_create_valid():
     assert obj.get('email') == 'jane.doe@gmail.com'
 
 @pytest.mark.integration()
-def test_create_invalid_missing():
+def test_create_invalid_missing(dao):
     data = [('firstName', 'John'), ('lastName', 'Smith')]
 
     with pytest.raises(WriteError):
         dao.create(data)
 
 @pytest.mark.integration()
-def test_create_invalid_misspelled():
+def test_create_invalid_misspelled(dao):
     data = [('frstName', 'John'), ('lastName', 'Smith'), ('email', 'john.smith@gmail.com')]
 
     with pytest.raises(WriteError):
